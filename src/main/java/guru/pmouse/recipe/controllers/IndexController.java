a999cc01a6254d50f22d3d97452f6baf467bcc25
mrmouseguru <50196887+mrmouseguru@users.noreply.github.com>
2019-12-07 15:18:35 +0700

    added index page and controller

---
+++ src/main/java/guru/pmouse/recipe/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.pmouse.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Guru Mouse on 12/07/2019
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
