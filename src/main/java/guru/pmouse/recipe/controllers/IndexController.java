4519acdbb89d336345037302da758c0b792422ab
mrmouseguru <50196887+mrmouseguru@users.noreply.github.com>
2019-12-17 06:59:56 +0700

    added one to one jpa relationship

--- src/main/java/guru/pmouse/recipe/controllers/IndexController.java
+++ src/main/java/guru/pmouse/recipe/controllers/IndexController.java
@@ -11,6 +11,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String getIndexPage(){
+        System.out.println("Some message to say ... 123333 5555");
         return "index";
     }
 }
