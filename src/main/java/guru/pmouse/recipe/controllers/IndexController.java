3328983a5e7a414f1e52771730897e5991ecfc8b
mrmouseguru <50196887+mrmouseguru@users.noreply.github.com>
2019-12-20 10:44:05 +0700

    added spring data jpa query methods

--- src/main/java/guru/pmouse/recipe/controllers/IndexController.java
+++ src/main/java/guru/pmouse/recipe/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.pmouse.recipe.controllers;
 
+import guru.pmouse.recipe.domain.Category;
+import guru.pmouse.recipe.domain.UnitOfMeasure;
+import guru.pmouse.recipe.repositories.CategoryRepository;
+import guru.pmouse.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by Guru Mouse on 12/07/2019
@@ -9,9 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
     public String getIndexPage(){
-        System.out.println("Some message to say ... 123333 5555");
+
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id: " + category.get().getId());
+        System.out.println("UOM Id: " + unitOfMeasure.get().getId());
+
         return "index";
     }
 }
