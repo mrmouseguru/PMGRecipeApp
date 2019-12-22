5321a40c9ed6ce4296c2f20a85bca44cd07cc969
mrmouseguru <50196887+mrmouseguru@users.noreply.github.com>
2019-12-22 12:40:49 +0700

    added Mockito Mocks Test for RecipeServiceImpl

---
+++ src/test/java/guru/pmouse/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package guru.pmouse.recipe.services;
+
+import guru.pmouse.recipe.domain.Recipe;
+import guru.pmouse.recipe.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by PMouse Guru  on 12/22/2019
+ */
+class RecipeServiceImplTest {
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
